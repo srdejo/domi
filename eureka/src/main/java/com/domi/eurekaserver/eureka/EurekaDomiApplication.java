ba67670d39fb5d80bfe26b0bad29cd222dfc3a91
srdejo_wposs <danielovallos@wposs.com>
2021-01-12 14:05:00 -0500

    Configuracion eureka server

--- eureka/src/main/java/com/domi/eurekaserver/eureka/EurekaDomiApplication.java
+++ eureka/src/main/java/com/domi/eurekaserver/eureka/EurekaDomiApplication.java
@@ -2,7 +2,9 @@ package com.domi.eurekaserver.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaDomiApplication {
 
