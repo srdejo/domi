08a0f81e6cec5d1721ce70b27702ad732806202f
srdejo_wposs <danielovallos@wposs.com>
2021-01-12 14:01:28 -0500

    Servicio de Spring Cloud Netflix: Eureka

---
+++ eureka/src/main/java/com/domi/eurekaserver/eureka/EurekaDomiApplication.java
@@ -0,0 +1,13 @@
+package com.domi.eurekaserver.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDomiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDomiApplication.class, args);
+	}
+
+}
---
+++ eureka/src/test/java/com/domi/eurekaserver/eureka/EurekaDomiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.eurekaserver.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDomiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
